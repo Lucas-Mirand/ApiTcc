b422a6de5b48a228834fb1d57da94706f9227c2a
Lucas Miranda de Souza <lucasmirandatrassato@gmail.com>
2025-11-17 09:24:25 -0300

    Remove Dotenv initialization from TccApplication
    Removed Dotenv configuration and loading from main method.

--- src/main/java/com/example/apitcc/TccApplication.java
+++ src/main/java/com/example/apitcc/TccApplication.java
@@ -1,6 +1,5 @@
 package com.example.apitcc;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TccApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-            .ignoreIfMalformed()
-            .load();
-
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
         SpringApplication.run(TccApplication.class, args);
     }
 }
